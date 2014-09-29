926a9d4d25dfdafd24e69d6762a11706fcf980b2
Nkelliny <sitelegion@coolsite.net>
2014-09-29 09:21:47 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 <?php
 
 
-	( $db = mysql_connect('sql2.njit.edu', 'ko45', 'obscene78') ) 
+	( $db = mysql_connect('sql2.njit.edu', 'ko45', 'XXXXXX') ) 
 		OR DIE ("Cannot conecct to database");
 		
 	mysql_select_db('ko45');
